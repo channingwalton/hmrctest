4e4ad08dc185eeddfdf18587a177ef4dbe22ae2e
Channing Walton <channingwalton@mac.com>
2019-08-10 12:01:10 +0100

    Deal with missing prices

--- src/main/scala/io/channing/CostCalculator.scala
+++ src/main/scala/io/channing/CostCalculator.scala
@@ -1,6 +1,11 @@
 package io.channing
 
 object CostCalculator {
-  def costs(input: List[String], prices: Map[String, Double]): Double =
-    input.foldLeft(0.0){ case (total, item) => total + prices(item) }
+  def costs(input: List[String], prices: Map[String, Double]): Either[String, Double] = {
+    val itemsMissingPrice = input.filterNot(prices.keySet).toSet
+    if (itemsMissingPrice.nonEmpty)
+      Left(s"The following items have no price: ${itemsMissingPrice.mkString(", ")}")
+    else
+      Right(input.foldLeft(0.0){ case (total, item) => total + prices(item) })
+  }
 }
--- src/test/scala/io/channing/CostCalculatorSpec.scala
+++ src/test/scala/io/channing/CostCalculatorSpec.scala
@@ -8,7 +8,14 @@ class CostCalculatorSpec extends FreeSpec with MustMatchers {
     val input = List("Apple", "Apple", "Orange", "Apple")
     val costs = Map("Apple" -> 0.60, "Orange" -> 0.25)
 
-    CostCalculator.costs(input, costs) mustEqual 2.05
+    CostCalculator.costs(input, costs) mustEqual Right(2.05)
+  }
+
+  "Calculate cost when item is missing from prices list" in {
+    val input = List("Apple", "Apple", "Orange", "Apple")
+    val costs = Map("Orange" -> 0.25)
+
+    CostCalculator.costs(input, costs) mustEqual Left("The following items have no price: Apple")
   }
 
 }
