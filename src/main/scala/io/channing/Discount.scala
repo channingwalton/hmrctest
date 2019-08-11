8be760d007db6ce34add4884141d5d5c9803af02
Channing Walton <channingwalton@mac.com>
2019-08-11 07:50:35 +0100

    removed an unnecessary val

--- src/main/scala/io/channing/Discount.scala
+++ src/main/scala/io/channing/Discount.scala
@@ -5,9 +5,8 @@ sealed trait Discount extends Product with Serializable {
 }
 
 final case class NForMDiscount(item: String, n: Int, m: Int) extends Discount {
-  override def saving(items: List[String], prices: Map[String, Int]): Option[Saving] = {
-    val count = items.count(_ == item)
-    if (count < n)
+  override def saving(items: List[String], prices: Map[String, Int]): Option[Saving] =
+    if (items.count(_ == item) < n)
       None
     else {
       val normalPrice = n * prices(item)
@@ -15,5 +14,4 @@ final case class NForMDiscount(item: String, n: Int, m: Int) extends Discount {
       val saving = normalPrice - discountPrice
       Some(Saving(this, List.fill(n)(item), saving))
     }
-  }
 }
